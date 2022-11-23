0cef019c3b125e64e7867941ba985fe4b9c78a6f
FaizanOwais25 <24100119@lums.edu.pk>
2022-11-23 18:12:33 +0500

    new files added backend

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 import Signupform from './pages/signupform/Signupform';
+import Registerform from './pages/registerform/Registerform'
+
 
 function App() {
   return (
-    <div className="App">
-   <Signupform></Signupform>
-    </div> 
+    <div>
+    <BrowserRouter> 
+    <Routes>
+    <Route path="/login" element={<Signupform/>} />
+    <Route path="/register" element={<Registerform />} />
+    </Routes>
+    </BrowserRouter>
+    
+    {/* <Signupform></Signupform> */}
+    </div>
   );
 }
 
