28a3690d7d27bd0413fc5c0da79b63f4364cacac
AaqibbHasanie <24100233@lums.edu.pk>
2022-12-09 04:50:16 +0500

    lkj

--- clientapp/src/pages/gallery/Gallery.js
+++ clientapp/src/pages/gallery/Gallery.js
@@ -1,5 +1,22 @@
+<<<<<<< Updated upstream
 import React, { useState /*, useEffect*/ } from "react";
 import Images from "./images";
+=======
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Navigate, redirect } from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+} from "react-router-dom";
+
+// import Images from "./images";
+>>>>>>> Stashed changes
 const mystyle = {
   color: "white",
   textAlign: "center",
@@ -10,6 +27,7 @@ const mystyle = {
 
 // https://images.pexels.com/photos/35967/mini-cooper-auto-model-vehicle.jpg?cs=srgb&dl=pexels-pixabay-35967.jpg&fm=jpg
 
+<<<<<<< Updated upstream
 // function fetchImages(){
 
 //     //get images from server as a json array
@@ -40,6 +58,125 @@ export default function gallery() {
               src={img}
               alt="dog"
               onClick={() => setSelectedImg(img)}
+=======
+const Gallery = () => {
+  // export default function Gallery() {
+  const [Images, setImages] = useState([]);
+  const [selectedImg, setSelectedImg] = useState(null);
+  const [searchData, setSearchData] = useState("");
+
+  // function fetchData() {
+  //   const fetchData = async () => {
+  //     const resp = await axios.post("http://localhost:3000/gallery");
+  //     console.log(resp);
+  //     const { data } = resp;
+  //     const { result } = data;
+  //     setImages(result);
+  //   };
+  //   try {
+  //     fetchData();
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  //   // (<Search searchData={searchData} />)
+  //   // navigate("/Search");
+  //   //get images from server as a json array
+  // }
+
+  function fetchSearch() {
+    const fetchSearch = async () => {
+      // e.preventDefault();
+      const resp = await axios.post("http://localhost:3000/search", {
+        searchData: searchData,
+      });
+      console.log("output: ", resp);
+      console.log(resp);
+      const { data } = resp;
+      const { result } = data;
+      setImages(result);
+      // const { data } = resp;
+      // const { result } = data;
+      // setImages(result);
+    };
+    try {
+      fetchSearch();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  function returnToGallery() {
+    const fetchData = async () => {
+      const resp = await axios.post("http://localhost:3000/gallery");
+      console.log(resp);
+      const { data } = resp;
+      const { result } = data;
+      setImages(result);
+    };
+    try {
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await axios.post("http://localhost:3000/gallery");
+      console.log(resp);
+      const { data } = resp;
+      const { result } = data;
+      setImages(result);
+    };
+    try {
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    // n,
+    <div className="App">
+      <h1 style={mystyle}>Gallery</h1>
+      <h6 style={mystyle}>Search the gallery</h6>
+
+      <div>
+        {" "}
+        <input
+          class="form-control"
+          placeholder="search in lowercase: male kurta shalwar/ female kurta shalwar / male shalwar kameez / female shalwar kameez / male shalwar / female shalwar"
+          value={searchData}
+          onChange={(e) => setSearchData(e.target.value)}
+          type="search"
+        ></input>
+        <a>
+          {" "}
+          <button class="main-btn" type="submit" onClick={fetchSearch}>
+            search
+          </button>
+        </a>
+        <a>
+          <button onClick={returnToGallery}> Back to Gallery</button>
+        </a>
+      </div>
+
+      <br></br>
+      <h2 style={mystyle}>All Items</h2>
+
+      <div className="container">
+        <img src={selectedImg} className="selected" />
+        <div className="imgContainer">
+          {Images.map((img, index) => (
+            <img
+              style={{
+                border: selectedImg === img ? "4px solid purple" : "",
+              }}
+              key={index}
+              src={img}
+              // value={selectedImg}
+              // // alt="dog"
+              // onClick={() => setSelectedImg(img)}
+>>>>>>> Stashed changes
             />
           ))}
         </div>
@@ -49,6 +186,7 @@ export default function gallery() {
 
       <br></br>
       <br></br>
+<<<<<<< Updated upstream
 
       <h2 style={mystyle}>Female Kurtas</h2>
       <br></br>
@@ -72,3 +210,10 @@ export default function gallery() {
 }
 
 // export default gallery;
+=======
+    </div>
+  );
+};
+
+export default Gallery;
+>>>>>>> Stashed changes
