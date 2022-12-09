6c8a65a60d1929a17f415041b1a2eb6e73064430
abdullah129 <24100015@lums.edu.pk>
2022-12-09 22:55:12 +0500

    Update 24100119_db.js

--- backend/24100119_db.js
+++ backend/24100119_db.js
@@ -47,17 +47,21 @@ function createTable(CreateQuerry) {
 */
 
 var c1 ="create table grp16_website.customer( cust_id varchar(200) Primary Key, password varchar(255), email_id varchar (200), name varchar(200), house_adress varchar(255),phone varchar(255),update_key int)";
-
 var a1= "create table grp16_website.admin( admin_id varchar(200) Primary Key, password varchar(255))"
-var i1= "insert into grp16_website.admin values('abdullah','$2b$10$vyI6pWhW0NtWdvmfDLeIle3o.PiArrFMh/JHmUZk2tEr705fqibaG')"
-var s1= "create table grp16_website.product( product_id varchar(200) Primary Key, product_name varchar(255) not null, price varchar(255) not null, product_image varchar(255),category varchar(255),update_key int,active_bit int,prod_image varchar(255))"
+var s1= "create table grp16_website.product( product_id varchar(200) Primary Key, product_name varchar(255) not null, price varchar(255) not null, product_image varchar(255),category varchar(255),update_key int,active_bit int)"
 var inven= "create table grp16_website.inventory( product_id varchar(200) Primary Key, quantity int, category varchar(255),update_key int,active_bit int, Foreign Key (product_id) references grp16_website.product(product_id))"
 var shop= "create table grp16_website.shopping_cart( shoppingcart_id varchar(200) Primary Key,cust_id varchar(200) Primary Key, product_id varchar(255), quantity int,total_cost double ,update_key int,active_bit int, Foreign Key (product_id) references grp16_website.product(product_id))"
 var curennt_order= "create table grp16_website.current_order( cust_id varchar(200) Primary Key, product_id varchar(255),order_id varchar(255) not null AUTO_INCREMENT,order_status varchar(255), quantity int ,update_key int,active_bit int, Foreign Key (cust_id) references grp16_website.shopping_cart(cust_id), Foreign Key (product_id) references grp16_website.product(product_id))"
 var order_history="create table grp16_website.order_history( cust_id varchar(200) Primary Key,shoppingcart_id varchar(200), product_id varchar(255),order_id varchar(255), quantity int,data date ,total_cost double ,update_key int,active_bit int, Foreign Key (cust_id) references grp16_website.current_order(cust_id), Foreign Key (product_id) references grp16_website.product(product_id))"
-var password="create table grp16_website.password_reset( cust_id varchar(200) Primary Key, password varchar(255),sec_ques varchar(255),update_key int,active_bit int)"
+var password="create table grp16_website.password_reset( cust_id varchar(200) Primary Key, password varchar(255),sec_ques varchar(255),update_key int,active_bit int,Foreign Key (cust_id) references grp16_website.customer(cust_id))"
 var category="create table grp16_website.category(category_name varchar(255) Primary Key,update_key int,active_bit int,Foreign Key (category_name) references grp16_website.product(category))"
 var sales = "create table grp16_website.sales(date date,amount double,update_key int,active_bit int)"
+
+
+var i1= "insert into grp16_website.admin values('abdullah','$2b$10$vyI6pWhW0NtWdvmfDLeIle3o.PiArrFMh/JHmUZk2tEr705fqibaG')"
+var i2= "insert into grp16_website.product values('1','abc','100','https://media.istockphoto.com/id/1180368192/photo/indian-bridegroom-wears-ethnic-or-traditional-cloths.jpg?s=612x612&w=is&k=20&c=OIsuOBbGxODCW-L2eREtWJnGhHaZR9rVUZp2ml433c0=','abcefg','0','1')"
+var i3= "insert into grp16_website.inventory values('1','100','abcefg','0','1')"
+
 connectionString.connect( (error)=>
 {
     if(!error)
@@ -76,14 +80,25 @@ connectionString.connect( (error)=>
                     //call create table here using await like done below here.
                     await createTable(c1)
                     await createTable(a1)
-                    // await createTable(i1)
-                    // await createTable(s1)
-                    // await createTable(inven)
-                    // await createTable(shop)
-                    // await createTable(curennt_order)
-                    // await createTable(order_history)
-                    // await createTable(password)
+                    await createTable(i1)
+                    await createTable(s1)
+                    await createTable(inven)
+                    await createTable(shop)
+                    await createTable(curennt_order)
+                    await createTable(order_history)
+                    await createTable(password)
+                    await createTable(category)
+                    await createTable(sales)
                     console.log("All tables created");
+
+
+                    await createTable(i1) // admin entry added
+                    console.log("Admin entry added")
+                    await createTable(i2) // product entry added
+                    await createTable(i3) // inventory entry added
+
+
+                    
                 }
                 catch(err)
                 {
@@ -102,54 +117,54 @@ connectionString.connect( (error)=>
 
 
 
-  var shop =
-  "create table grp16_website.shopping_cart( cust_id varchar(200) Primary Key, product_id varchar(255), quantity varchar(255),total_cost double ,update_key int,active_bit int)";
-var curennt_order =
-  "create table grp16_website.current_order( cust_id varchar(200) Primary Key, product_id varchar(255),order_id varchar(255),order_status varchar(255), quantity varchar(255) ,update_key int,active_bit int)";
-var order_history =
-  "create table grp16_website.order_history( cust_id varchar(200) Primary Key, product_id varchar(255),order_id varchar(255), quantity varchar(255),data varchar(255),total_cost double ,update_key int,active_bit int)";
-var password =
-  "create table grp16_website.password_reset( cust_id varchar(200) Primary Key, password varchar(255),sec_ques varchar(255),update_key int,active_bit int)";
-connectionString.connect((error) => {
-  if (!error) {
-    console.log("Connection has been established");
-    connectionString.query(
-      `CREATE DATABASE IF NOT EXISTS ${process.env.database}`,
-      async (err2, result) => {
-        if (err2) {
-          console.log(err2);
-        } else {
-          console.log("Database Created");
-          try {
-            //call create table here using await like done below here.
-            await createTable(c1);
-            await createTable(a1);
-            await createTable(i1);
-            await createTable(s1);
-            await createTable(inven);
-            await createTable(shop);
-            await createTable(curennt_order);
-            await createTable(order_history);
-            await createTable(password);
-            console.log("All tables created");
-          } catch (err) {
-            // err=str
-            var str = err.sqlMessage;
-            if (str.includes("already exists"))
-              console.log("Nothing to worry about, table(s) already exist");
-            else console.log(err);
-          }
+//   var shop =
+//   "create table grp16_website.shopping_cart( cust_id varchar(200) Primary Key, product_id varchar(255), quantity varchar(255),total_cost double ,update_key int,active_bit int)";
+// var curennt_order =
+//   "create table grp16_website.current_order( cust_id varchar(200) Primary Key, product_id varchar(255),order_id varchar(255),order_status varchar(255), quantity varchar(255) ,update_key int,active_bit int)";
+// var order_history =
+//   "create table grp16_website.order_history( cust_id varchar(200) Primary Key, product_id varchar(255),order_id varchar(255), quantity varchar(255),data varchar(255),total_cost double ,update_key int,active_bit int)";
+// var password =
+//   "create table grp16_website.password_reset( cust_id varchar(200) Primary Key, password varchar(255),sec_ques varchar(255),update_key int,active_bit int)";
+// connectionString.connect((error) => {
+//   if (!error) {
+//     console.log("Connection has been established");
+//     connectionString.query(
+//       `CREATE DATABASE IF NOT EXISTS ${process.env.database}`,
+//       async (err2, result) => {
+//         if (err2) {
+//           console.log(err2);
+//         } else {
+//           console.log("Database Created");
+//           try {
+//             //call create table here using await like done below here.
+//             await createTable(c1);
+//             await createTable(a1);
+//             await createTable(i1);
+//             await createTable(s1);
+//             await createTable(inven);
+//             await createTable(shop);
+//             await createTable(curennt_order);
+//             await createTable(order_history);
+//             await createTable(password);
+//             console.log("All tables created");
+//           } catch (err) {
+//             // err=str
+//             var str = err.sqlMessage;
+//             if (str.includes("already exists"))
+//               console.log("Nothing to worry about, table(s) already exist");
+//             else console.log(err);
+//           }
 
-          /*
-                Here you will be calling the createTable function to create each table passing the above created
-                variable as a paramter to the function.
-                */
-          connectionString.end();
-        }
-      }
-    );
-  } else {
-    console.log("Connection failed");
-    console.log(error);
-  }
-});
+//           /*
+//                 Here you will be calling the createTable function to create each table passing the above created
+//                 variable as a paramter to the function.
+//                 */
+//           connectionString.end();
+//         }
+//       }
+//     );
+//   } else {
+//     console.log("Connection failed");
+//     console.log(error);
+//   }
+// });
