5b5464bbf53a46827ac127e557f8af3860f0913b
HaadZ <71570298+ItzHaad@users.noreply.github.com>
2022-12-10 10:19:51 +0500

    Update 24100119_db.js

--- backend/24100119_db.js
+++ backend/24100119_db.js
@@ -46,15 +46,24 @@ function createTable(CreateQuerry) {
 
 */
 
-var c1 ="create table grp16_website.customer( cust_id varchar(200) Primary Key, password varchar(255), email_id varchar (200), name varchar(200), house_adress varchar(255),phone varchar(255),update_key int)";
-var a1= "create table grp16_website.admin( admin_id varchar(200) Primary Key, password varchar(255))"
-var s1= "create table grp16_website.product( product_id varchar(200) Primary Key, product_name varchar(255) not null, price varchar(255) not null, product_image varchar(255),category varchar(255),update_key int,active_bit int)"
-var inven= "create table grp16_website.inventory( product_id varchar(200) Primary Key, quantity int, category varchar(255),update_key int,active_bit int, Foreign Key (product_id) references grp16_website.product(product_id))"
-var shop= "create table grp16_website.shopping_cart( shoppingcart_id varchar(200) Primary Key,cust_id varchar(200) Primary Key, product_id varchar(255), quantity int,total_cost double ,update_key int,active_bit int, Foreign Key (product_id) references grp16_website.product(product_id))"
-var curennt_order= "create table grp16_website.current_order( cust_id varchar(200) Primary Key, product_id varchar(255),order_id varchar(255) not null AUTO_INCREMENT,order_status varchar(255), quantity int ,update_key int,active_bit int, Foreign Key (cust_id) references grp16_website.shopping_cart(cust_id), Foreign Key (product_id) references grp16_website.product(product_id))"
-var order_history="create table grp16_website.order_history( cust_id varchar(200) Primary Key,shoppingcart_id varchar(200), product_id varchar(255),order_id varchar(255), quantity int,data date ,total_cost double ,update_key int,active_bit int, Foreign Key (cust_id) references grp16_website.current_order(cust_id), Foreign Key (product_id) references grp16_website.product(product_id))"
-var password="create table grp16_website.password_reset( cust_id varchar(200) Primary Key, password varchar(255),sec_ques varchar(255),update_key int,active_bit int,Foreign Key (cust_id) references grp16_website.customer(cust_id))"
-var category="create table grp16_website.category(category_name varchar(255) Primary Key,update_key int,active_bit int,Foreign Key (category_name) references grp16_website.product(category))"
+var c1 = "create table grp16_website.customer( cust_id varchar(200) Primary Key, password varchar(255), email_id varchar (200), name varchar(200), house_adress varchar(255),phone varchar(255),update_key int)";
+
+var a1 = "create table grp16_website.admin( admin_id varchar(200) Primary Key, password varchar(255))"
+
+var s1 = "create table grp16_website.product( product_id varchar(200) UNIQUE, product_name varchar(255) not null, price varchar(255) not null, product_image varchar(255),category varchar(255) KEY,update_key int,active_bit int)"
+
+var inven = "create table grp16_website.inventory( product_id varchar(200) Primary Key, quantity int, category varchar(255),update_key int,active_bit int, Foreign Key (product_id) references grp16_website.product(product_id))"
+
+var shop = "create table grp16_website.shopping_cart( shoppingcart_id varchar(200) Primary Key,cust_id varchar(200) UNIQUE, product_id varchar(255), quantity int,total_cost double ,update_key int,active_bit int, Foreign Key (product_id) references grp16_website.product(product_id))"
+
+var current_order = "create table grp16_website.current_order( cust_id varchar(200) UNIQUE, product_id varchar(255), order_id int not null AUTO_INCREMENT Primary Key,order_status varchar(255), quantity int ,update_key int,active_bit int, Foreign Key (cust_id) references grp16_website.shopping_cart(cust_id), Foreign Key (product_id) references grp16_website.product(product_id))"
+
+var order_history = "create table grp16_website.order_history( cust_id varchar(200) Primary Key,shoppingcart_id varchar(200), product_id varchar(255),order_id int, quantity int,data date ,total_cost double ,update_key int,active_bit int, Foreign Key (cust_id) references grp16_website.current_order(cust_id), Foreign Key (product_id) references grp16_website.product(product_id), Foreign Key (order_id) references grp16_website.current_order(order_id))"
+
+var password = "create table grp16_website.password_reset( cust_id varchar(200) Primary Key, password varchar(255),sec_ques varchar(255),update_key int,active_bit int,Foreign Key (cust_id) references grp16_website.customer(cust_id))"
+
+var category = "create table grp16_website.category(category_name varchar(255) Primary Key,update_key int,active_bit int,Foreign Key (category_name) references grp16_website.product(category))"
+
 var sales = "create table grp16_website.sales(date date,amount double,update_key int,active_bit int)"
 
 
@@ -80,11 +89,11 @@ connectionString.connect( (error)=>
                     //call create table here using await like done below here.
                     await createTable(c1)
                     await createTable(a1)
-                    await createTable(i1)
+                    //await createTable(i1)
                     await createTable(s1)
                     await createTable(inven)
                     await createTable(shop)
-                    await createTable(curennt_order)
+                    await createTable(current_order)
                     await createTable(order_history)
                     await createTable(password)
                     await createTable(category)
@@ -96,21 +105,34 @@ connectionString.connect( (error)=>
                     console.log("Admin entry added")
                     await createTable(i2) // product entry added
                     await createTable(i3) // inventory entry added
+               
+                        try {
+                            //call create table here using await like done below here.
+                            //await createTable()
+                            connectionString.query("INSERT INTO grp16_website.product(product_id, product_name, price, product_image, category, update_key, active_bit) VALUES('2', 'ndsjf', '34', 'https://cdn.shopify.com/s/files/1/0347/0904/5292/files/ek0501_1_1_720x_2fc240cc-61df-4370-ad86-45734bf5699c_480x480.jpg?v=1618310069', 'male', 1, 1)");
+                        }
+                        catch (err) {
+                            console.log(err);
+                        }
 
-
-                    
                 }
                 catch(err)
                 {
                     // err=str
-                    var str=err.sqlMessage
-                    if (str.includes("already exists"))
-                        console.log("Nothing to worry about, table(s) already exist")
-                    else
-                        console.log(err)
+                    //var str=err.sqlMessage
+                    //if (str.includes("already exists"))
+                    //    console.log("Nothing to worry about, table(s) already exist")
+                    //else
+                    //    console.log(err)
+                    console.log(err)
                 }
+                connectionString.end();
             }
         })
+    }
+    else {
+        console.log("Connection failed");
+        console.log(error);
     }
 })
     
